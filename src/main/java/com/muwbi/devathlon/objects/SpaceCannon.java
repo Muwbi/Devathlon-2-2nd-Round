afc13eba519da650a91df3bd52c24c56e1835285
Muwbi <muwbi@hotmail.com>
2015-07-17 19:14:31 +0200

    Change SpaceCannon

--- src/main/java/com/muwbi/devathlon/objects/SpaceCannon.java
+++ src/main/java/com/muwbi/devathlon/objects/SpaceCannon.java
@@ -3,24 +3,20 @@ package com.muwbi.devathlon.objects;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.WrappedDataWatcher;
+
 import com.muwbi.devathlon.SpaceFighter;
-import com.muwbi.devathlon.util.WrapperPlayServerEntityMetadata;
-import lombok.RequiredArgsConstructor;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
+
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.plugin.Plugin;
-import org.spigotmc.event.entity.EntityDismountEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+
+
+import java.util.UUID;
 
 /**
  * Created by Muwbi
@@ -29,8 +25,8 @@ public class SpaceCannon extends PacketAdapter implements Listener {
 
     private static final Material BLOCK_MATERIAL = Material.COBBLESTONE_STAIRS;
 
-    private int entityId;
     private boolean isInUse;
+    private UUID uuid;
 
     public SpaceCannon() {
         super( SpaceFighter.getInstance(), PacketType.Play.Server.ENTITY_METADATA );
@@ -40,53 +36,29 @@ public class SpaceCannon extends PacketAdapter implements Listener {
 
     @EventHandler
     public void onPlayerInteract( PlayerInteractEvent event ) {
-        if ( event.getAction() == Action.RIGHT_CLICK_BLOCK ) {
-            makePlayerSitOnBlock( event.getPlayer(), event.getClickedBlock() );
-        }
-    }
+        if ( event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock().getType() == BLOCK_MATERIAL ) {
+            if ( !isInUse ) {
+                isInUse = true;
+                uuid = event.getPlayer().getUniqueId();
 
-    @EventHandler
-    public void onEntityDismount( EntityDismountEvent event ) {
-        if ( event.getDismounted().getType() == EntityType.ARROW ) {
-            event.getDismounted().remove();
-        }
-    }
-
-    @Override
-    public void onPacketSending( PacketEvent event ) {
-        if ( event.getPacketType() == PacketType.Play.Server.ENTITY_METADATA ) {
-            Bukkit.broadcastMessage( "Sending Entity Metadata packet" );
-
-            WrapperPlayServerEntityMetadata packet = new WrapperPlayServerEntityMetadata( event.getPacket() );
-
-            Bukkit.broadcastMessage( "" + packet.getEntityId() );
-            Bukkit.broadcastMessage( "" + entityId );
-
-            if ( packet.getEntity( Bukkit.getWorlds().get( 0 ) ).getType() == EntityType.ARROW ) {
-                WrappedDataWatcher dataWatcher = new WrappedDataWatcher( packet.getEntityMetadata() );
-
-                Byte flag = dataWatcher.getByte( 0 );
-
-                if ( flag != null ) {
-                    Bukkit.broadcastMessage( "Modifying entity metadata for entity #" + packet.getEntityId() );
-
-                    packet = new WrapperPlayServerEntityMetadata( packet.getHandle().deepClone() );
-                    dataWatcher = new WrappedDataWatcher( packet.getEntityMetadata() );
-                    dataWatcher.setObject( 0, (byte) ( flag | 0x20 ) );
-
-                    event.setPacket( packet.getHandle() );
-                }
+                event.getPlayer().sendMessage( "Du benutzt nun die SpaceCannon" );
             }
         }
     }
 
-    private void makePlayerSitOnBlock( Player player, Block block ) {
-        Bukkit.broadcastMessage( player.getName() + " sat down" );
+    @EventHandler
+    public void onPlayerMove( PlayerMoveEvent event ) {
+        if ( uuid == event.getPlayer().getUniqueId() ) {
+            event.setCancelled( true );
+        }
+    }
 
-        Entity entity = player.getWorld().spawnEntity( block.getLocation(), EntityType.ARROW );
-        entity.setPassenger( player );
-
-        entityId = entity.getEntityId();
+    @EventHandler
+    public void onPlayerToggleSneak( PlayerToggleSneakEvent event ) {
+        if ( uuid == event.getPlayer().getUniqueId() && event.isSneaking() ) {
+            isInUse = false;
+            uuid = null;
+        }
     }
 
 }
